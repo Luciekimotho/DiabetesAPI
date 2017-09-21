1a302196d84abe845fe029a11c615c0c68f53476
Luciekimotho <luciekimotho@gmail.com>
2017-09-21 13:45:05 +0300

    super user

--- diabetes/admin.py
+++ diabetes/admin.py
@@ -6,9 +6,3 @@ from .models import Profile, Readings
 admin.site.register(Profile)
 admin.site.register(Readings)
 
-if "ADMIN_USER" in os.environ and "ADMIN_PASSWORD" in os.environ:
-    from django.contrib.auth.models import User
-    user=User.objects.create_user(os.environ['ADMIN_USER'], password=os.environ['ADMIN_PASSWORD'])
-    user.is_superuser=True
-    user.is_staff=True
-    user.save()
